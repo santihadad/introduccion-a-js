2e34b0ff4135151cdb9e5218b1226a4ab500c9e3
Santiago Hadad <santihadad.sh@hotmail.com>
2020-02-06 18:30:13 -0300

    Consignas
    copia de consignas en los archivos correspondientes

---
+++ tareas/clase-6/tarea1.js
@@ -0,0 +1,8 @@
+/*
+TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
+Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
+Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
+
+Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
+*/
+
